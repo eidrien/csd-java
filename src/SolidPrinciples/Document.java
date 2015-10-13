e1355bcfc75a191cb100fafa63e587e15a310f5e
Alberto <alb.gomez@gmail.com>
2015-10-13 18:31:07 +0200

    Creando la clase base y su primer refactor

---
+++ src/SolidPrinciples/Document.java
@@ -0,0 +1,26 @@
+package SolidPrinciples;
+
+
+public class Document {
+
+    private String title;
+    private String text;
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+    public String getText()
+    {
+        return text;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+}
