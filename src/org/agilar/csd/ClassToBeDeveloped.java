3e9d70b560321cab51345b181fc130687a33708f
Adrian Perreau de Pinninck <aperreau+git@gmail.com>
2015-10-19 13:42:36 +0200

    Added initial code for testing, mocking, and FIRST Added libraries needed.

---
+++ src/org/agilar/csd/ClassToBeDeveloped.java
@@ -0,0 +1,10 @@
+package org.agilar.csd;
+
+public class ClassToBeDeveloped {
+
+	public boolean unimplementedMethod() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ test/org/agilar/csd/ExamplesOfFirstPrinciples.java
@@ -0,0 +1,106 @@
+package org.agilar.csd;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExamplesOfFirstPrinciples {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/////////////////// FAST //////////////////////
+	@Test
+	public void hundredsOfTestsShouldRunPerMinute() {
+		Date startTime = new Date();
+		assertEquals(1,1);
+		Date endTime = new Date();
+		long elapsedTime = endTime.getTime() - startTime.getTime();
+		assertTrue(elapsedTime < 50);
+	}
+	
+	@Test
+	public void longTestsAreNotUnitTests(){
+		Date startTime = new Date();
+		for(long i=0; i<10000; i++){
+			for(long j=0; j<1000; j++){
+				String message = "When i=" + i + " and j=" + j + "the multiplication is less than 0";
+				assertFalse(message, j*i < 0);
+			}
+		}
+		Date endTime = new Date();
+		long elapsedTime = endTime.getTime() - startTime.getTime();
+		assertFalse(elapsedTime < 250);
+	}
+	
+	/////////////////// ISOLATED/INDEPENDENT //////////////////////
+	@Test
+	public void testsWhereBrokenBehaviourIsNotClearAreBad(){
+		Calculator calculator = new Calculator();
+		calculator.add(5);
+		calculator.subtract(3);
+		calculator.multiplyBy(12);
+		calculator.add(10);
+		calculator.divideBy(2);
+		assertEquals("I have no idea what broke", 17, calculator.getValue());
+	}
+	
+	@Test
+	public void testShouldIsolateTheBehaviourThatBroke(){
+		Calculator calculator = new Calculator();
+		assertEquals("Initial value of calculator should be 0", 0, calculator.getValue());
+	}
+
+	@Test
+	public void theSystemUnderTestShouldBeASingleClass(){
+		
+	}
+	
+	@Test
+	public void runningTestsInDifferentOrderShouldStillWork(){
+		
+	}
+	
+	/////////////////// REPEATABLE //////////////////////
+	@Test
+	public void testsShouldHaveTheSameResultIndependentlyOfExternalContext(){
+
+	}
+	
+	@Test
+	public void testsShouldNotHaveSideEffects(){
+		
+	}
+	
+	@Test
+	public void testsShouldNotDependOnExternalServices(){
+		//Example with date
+	}
+
+	/////////////////// SELF-VALIDATING //////////////////////
+	@Test
+	public void testsShouldBeSelfValidating(){
+		
+	}
+	
+	@Test
+	public void testsShouldNotUsePrintoutsOrLogThings(){
+		
+	}
+	
+	/////////////////// TIMELY //////////////////////
+	@Test
+	public void testsShouldBeWritenBeforeTheCode(){
+		ClassToBeDeveloped sut = new ClassToBeDeveloped();
+		assertTrue(sut.unimplementedMethod());
+	}
+}
