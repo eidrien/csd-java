0be1d4bbf435d905587d79a802fa3bc9a10c3dbd
Alberto <alb.gomez@gmail.com>
2015-10-21 16:57:51 -0200

    ServiceLocator with IoC Guice!!

--- src/org/agilar/csd/solid/DIP/Manual/Main.java
+++ src/org/agilar/csd/solid/DIP/Manual/Main.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.agilar.csd.solid.DIP.ServiceLocator.FormatConverterModule;
 import org.xml.sax.SAXException;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 
 public class Main {
 
@@ -17,16 +21,20 @@ public class Main {
 		// String sourceFileName = "http://chris.59north.com/Document1.xml";
 		String targetFileName = "https://agilarcsd.blob.core.windows.net/agilarcsdcontainer/Document1.json";
 
-		InputParser jsonInputParser = new JsonInputParser();
-		DocumentSerializer documentSerializer = new CamelCaseJsonSerializer();
+		//InputParser jsonInputParser = new JsonInputParser();
+		//DocumentSerializer documentSerializer = new CamelCaseJsonSerializer();
 		
-		FormatConverter formatConverter = new FormatConverter(jsonInputParser,documentSerializer);
-		DocumentStorageFactory documentStorageFactory = new DocumentStorageFactory();
+		 Injector injector = Guice.createInjector(new FormatConverterModule());
+		 FormatConverter formatConverter = injector.getInstance(FormatConverter.class);
+		
+	     DocumentStorageFactory documentStorageFactory = new DocumentStorageFactory();
 		
 		if (!formatConverter.ConvertFormat(documentStorageFactory,sourceFileName,targetFileName)) {
 			System.out.println("Conversion failed...");
 		} else
 			System.out.println("Conversion done...");
 	}
+	
+	
 
 }
