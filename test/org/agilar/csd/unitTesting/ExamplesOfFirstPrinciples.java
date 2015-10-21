d3e6b79baea66cd11ade1b83db29cbdc56575d42
Adrian Perreau de Pinninck <aperreau+git@gmail.com>
2015-10-21 15:43:43 +0200

    Correct formatting for file

--- test/org/agilar/csd/unitTesting/ExamplesOfFirstPrinciples.java
+++ test/org/agilar/csd/unitTesting/ExamplesOfFirstPrinciples.java
@@ -22,29 +22,29 @@ public class ExamplesOfFirstPrinciples {
 	@Test
 	public void hundredsOfTestsShouldRunPerMinute() {
 		Date startTime = new Date();
-		assertEquals(1,1);
+		assertEquals(1, 1);
 		Date endTime = new Date();
 		long elapsedTime = endTime.getTime() - startTime.getTime();
 		assertTrue(elapsedTime < 50);
 	}
-	
+
 	@Test
-	public void longTestsAreNotUnitTests(){
+	public void longTestsAreNotUnitTests() {
 		Date startTime = new Date();
-		for(long i=0; i<10000; i++){
-			for(long j=0; j<1000; j++){
+		for (long i = 0; i < 10000; i++) {
+			for (long j = 0; j < 1000; j++) {
 				String message = "When i=" + i + " and j=" + j + "the multiplication is less than 0";
-				assertFalse(message, j*i < 0);
+				assertFalse(message, j * i < 0);
 			}
 		}
 		Date endTime = new Date();
 		long elapsedTime = endTime.getTime() - startTime.getTime();
 		assertFalse(elapsedTime < 250);
 	}
-	
+
 	/////////////////// ISOLATED/INDEPENDENT //////////////////////
 	@Test
-	public void testsWhereBrokenBehaviourIsNotClearAreBad(){
+	public void testsWhereBrokenBehaviourIsNotClearAreBad() {
 		Calculator calculator = new Calculator();
 		calculator.add(5);
 		calculator.subtract(3);
@@ -53,53 +53,53 @@ public class ExamplesOfFirstPrinciples {
 		calculator.divideBy(2);
 		assertEquals("I have no idea what broke", 17, calculator.getValue());
 	}
-	
+
 	@Test
-	public void testShouldIsolateTheBehaviourThatBroke(){
+	public void testShouldIsolateTheBehaviourThatBroke() {
 		Calculator calculator = new Calculator();
 		assertEquals("Initial value of calculator should be 0", 0, calculator.getValue());
 	}
 
 	@Test
-	public void theSystemUnderTestShouldBeASingleClass(){
-		
-	}
-	
-	@Test
-	public void runningTestsInDifferentOrderShouldStillWork(){
-		
-	}
-	
-	/////////////////// REPEATABLE //////////////////////
-	@Test
-	public void testsShouldHaveTheSameResultIndependentlyOfExternalContext(){
+	public void theSystemUnderTestShouldBeASingleClass() {
 
 	}
-	
+
 	@Test
-	public void testsShouldNotHaveSideEffects(){
-		
+	public void runningTestsInDifferentOrderShouldStillWork() {
+
 	}
-	
+
+	/////////////////// REPEATABLE //////////////////////
 	@Test
-	public void testsShouldNotDependOnExternalServices(){
-		//Example with date
+	public void testsShouldHaveTheSameResultIndependentlyOfExternalContext() {
+
+	}
+
+	@Test
+	public void testsShouldNotHaveSideEffects() {
+
+	}
+
+	@Test
+	public void testsShouldNotDependOnExternalServices() {
+		// Example with date
 	}
 
 	/////////////////// SELF-VALIDATING //////////////////////
 	@Test
-	public void testsShouldBeSelfValidating(){
-		
+	public void testsShouldBeSelfValidating() {
+
 	}
-	
+
 	@Test
-	public void testsShouldNotUsePrintoutsOrLogThings(){
-		
+	public void testsShouldNotUsePrintoutsOrLogThings() {
+
 	}
-	
+
 	/////////////////// TIMELY //////////////////////
 	@Test
-	public void testsShouldBeWritenBeforeTheCode(){
+	public void testsShouldBeWritenBeforeTheCode() {
 		ClassToBeDeveloped sut = new ClassToBeDeveloped();
 		assertTrue(sut.unimplementedMethod());
 	}
