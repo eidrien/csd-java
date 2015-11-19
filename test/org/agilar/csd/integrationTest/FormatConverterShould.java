85d36490d4126d4d9c627d42fabf4e2d0601408d
Alberto <alb.gomez@gmail.com>
2015-11-19 10:16:41 +0100

    resolve conflicts with Maven

--- test/org/agilar/csd/integrationTest/FormatConverterShould.java
+++ test/org/agilar/csd/integrationTest/FormatConverterShould.java
@@ -1,61 +1,47 @@
 package org.agilar.csd.integrationTest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.junit.Test;
-import org.xml.sax.SAXException;
-
-import junit.framework.AssertionFailedError;
-
-import org.agilar.csd.solid.DIP.Manual.CamelCaseJsonSerializer;
 import org.agilar.csd.solid.DIP.Manual.DocumentSerializer;
 import org.agilar.csd.solid.DIP.Manual.DocumentStorageFactory;
 import org.agilar.csd.solid.DIP.Manual.FormatConverter;
 import org.agilar.csd.solid.DIP.Manual.InputParser;
 import org.agilar.csd.solid.DIP.Manual.JsonDocumentSerializer;
-import org.agilar.csd.solid.DIP.Manual.JsonInputParser;
+import org.junit.Test;
+import org.xml.sax.SAXException;
 
 public class FormatConverterShould {
 
 	@Test
-	public void InputXmlFileSystemOutputJsonFileSystem() {
+	public void Input_Xml_by_FileSystem_Output_Json_To_FileSystem()
+			throws IOException, SAXException, ParserConfigurationException {
 		String current;
-		String sourceFileName;
-		String targetFileName;
-	
-		try {
-			 current = new File(".").getCanonicalPath();
-			 sourceFileName = current + "/Input Documents/Document1.xml";
-		     targetFileName = current + "/Output Documents/Document1.json";
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			fail();
-		}
-		
-		
+		String sourceFileName = null;
+		String targetFileName = null;
+
+		current = new File(".").getCanonicalPath();
+		sourceFileName = current + "/Input Documents/Document1.xml";
+		targetFileName = current + "/Output Documents/Document1.json";
+
 		InputParser xmlInputParser = new InputParser();
 		DocumentSerializer documentSerializer = new JsonDocumentSerializer();
-		
-		FormatConverter formatConverter = new FormatConverter(xmlInputParser,documentSerializer);
+
+		FormatConverter formatConverter = new FormatConverter(xmlInputParser, documentSerializer);
 		DocumentStorageFactory documentStorageFactory = new DocumentStorageFactory();
-		boolean fileConverted;
-		try {
-			fileConverted = formatConverter.ConvertFormat(documentStorageFactory,sourceFileName,targetFileName);
-		} catch (IOException | SAXException | ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		boolean fileConverted = false;
+
+		fileConverted = formatConverter.ConvertFormat(documentStorageFactory, sourceFileName, targetFileName);
+
 		File fileOutput = new File(targetFileName);
-			
+
 		assertEquals(true, fileConverted);
-		assertEquals(true,fileOutput.exists());
-		
-		
+		assertEquals(true, fileOutput.exists());
+
 	}
 
 }
