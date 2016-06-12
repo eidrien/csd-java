00eec8d7ccc569922c37926b7c299e16d1624b6f
Adrian Perreau de Pinninck <aperreau+git@gmail.com>
2016-06-12 14:42:41 +0200

    Fixed Gilded Rose tests

--- src/org/agilar/csd/refactoring/gildedRose/GildedRose.java
+++ src/org/agilar/csd/refactoring/gildedRose/GildedRose.java
@@ -1,91 +1,106 @@
 package org.agilar.csd.refactoring.gildedRose;
-
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class GildedRose {
 
-	
-	public GildedRose(){
-		List<Item> items = new ArrayList<Item>();
-		items.add(new Item("+5 Dexterity Vest", 10, 20));
-		  items.add(new Item("Aged Brie", 2, 0));
-		  items.add(new Item("Elixir of the Mongoose", 5, 7));
-		  items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
-		  items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
-		  items.add(new Item("Conjured Mana Cake", 3, 6));
-		  updateQuality(items);
-	}
+	private static List<Item> items = null;
 
-	public List<Item> updateQuality(List<Item> items) {
-		  for (int i = 0; i < items.size(); i++) {
-			    if ("Aged Brie" != items.get(i).name && "Backstage passes to a TAFKAL80ETC concert" != items.get(i).name) {
-			      //TODO: Improve this code.
-			      if (items.get(i).quality > 0) {
-			        if ("Sulfuras, Hand of Ragnaros" != items.get(i).name) {
-			          items.get(i).quality = items.get(i).quality - 1;
-			        }
-			      }
-			    } else {
-			      if (items.get(i).quality < 50) {
-			        items.get(i).quality = items.get(i).quality + 1;
-			        if ("Aged Brie" == items.get(i).name) {
-			            if (items.get(i).sellIn < 6) {
-			              items.get(i).quality = items.get(i).quality + 1;
-			            }
-			        }
-			        //Increases the Quality of the stinky cheese if its 11 days to due date.
-			        if ("Aged Brie" == items.get(i).name) {
-			            if (items.get(i).sellIn < 11) {
-			              items.get(i).quality = items.get(i).quality + 1;
-			            }
-			        }
-			        if ("Backstage passes to a TAFKAL80ETC concert" == items.get(i).name) {
-			          if (items.get(i).sellIn < 11) {
-			            // See revision number 2394 on SVN.
-			            if (items.get(i).quality < 50) {
-			              items.get(i).quality = items.get(i).quality + 1;
-			            }
-			          }
-			          //Increases the Quality of Backstage Passes if the Quality is 6 or less.
-			          if (items.get(i).sellIn < 6) {
-			            if (items.get(i).quality < 50) {
-			              items.get(i).quality = items.get(i).quality + 1;
-			            }
-			          }
-			        }
-			      }
-			    }
-			    if ("Sulfuras, Hand of Ragnaros" != items.get(i).name) {
-			      items.get(i).sellIn = items.get(i).sellIn - 1;
-			    }
-			    if (items.get(i).sellIn < 0) {
-			      if ("Aged Brie" != items.get(i).name) {
-			        if ("Backstage passes to a TAFKAL80ETC concert" != items.get(i).name) {
-			          if (items.get(i).quality > 0) {
-			            if ("Sulfuras, Hand of Ragnaros" != items.get(i).name) {
-			              items.get(i).quality = items.get(i).quality - 1;
-			            }
-			          }
-			        } else {
-			          //TODO: Fix this.
-			          items.get(i).quality = items.get(i).quality - items.get(i).quality;
-			        }
-			      } else {
-			        if (items.get(i).quality < 50) {
-			          items.get(i).quality = items.get(i).quality + 1;
-			        }
-			        if ("Aged Brie" == items.get(i).name && items.get(i).sellIn <= 0)
-			            items.get(i).quality = 0;
-			      } // of for.
-			    }
-			    if ("Sulfuras, Hand of Ragnaros" != items.get(i).name)
-			      if (items.get(i).quality > 50) items.get(i).quality = 50;
-			  }
-			  return items;
-	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+        System.out.println("OMGHAI!");
+		
+        items = new ArrayList<Item>();
+        items.add(new Item("+5 Dexterity Vest", 10, 20));
+        items.add(new Item("Aged Brie", 2, 0));
+        items.add(new Item("Elixir of the Mongoose", 5, 7));
+        items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        items.add(new Item("Conjured Mana Cake", 3, 6));
+
+        updateQuality(items);
 }
 
-/*
-	};
-*/
+
+	
+    public static void updateQuality(List<Item> items)
+    {
+        for (int i = 0; i < items.size(); i++)
+        {
+            if ((!"Aged Brie".equals(items.get(i).getName())) && !"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName())) 
+            {
+                if (items.get(i).getQuality() > 0)
+                {
+                    if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+                    {
+                        items.get(i).setQuality(items.get(i).getQuality() - 1);
+                    }
+                }
+            }
+            else
+            {
+                if (items.get(i).getQuality() < 50)
+                {
+                    items.get(i).setQuality(items.get(i).getQuality() + 1);
+
+                    if ("Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+                    {
+                        if (items.get(i).getSellIn() < 11)
+                        {
+                            if (items.get(i).getQuality() < 50)
+                            {
+                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                            }
+                        }
+
+                        if (items.get(i).getSellIn() < 6)
+                        {
+                            if (items.get(i).getQuality() < 50)
+                            {
+                                items.get(i).setQuality(items.get(i).getQuality() + 1);
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+            {
+                items.get(i).setSellIn(items.get(i).getSellIn() - 1);
+            }
+
+            if (items.get(i).getSellIn() < 0)
+            {
+                if (!"Aged Brie".equals(items.get(i).getName()))
+                {
+                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(items.get(i).getName()))
+                    {
+                        if (items.get(i).getQuality() > 0)
+                        {
+                            if (!"Sulfuras, Hand of Ragnaros".equals(items.get(i).getName()))
+                            {
+                                items.get(i).setQuality(items.get(i).getQuality() - 1);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        items.get(i).setQuality(items.get(i).getQuality() - items.get(i).getQuality());
+                    }
+                }
+                else
+                {
+                    if (items.get(i).getQuality() < 50)
+                    {
+                        items.get(i).setQuality(items.get(i).getQuality() + 1);
+                    }
+                }
+            }
+        }
+    }
+
+}
--- test/org/agilar/csd/refactoring/gildedRose/GildedRoseShould.java
+++ test/org/agilar/csd/refactoring/gildedRose/GildedRoseShould.java
@@ -30,52 +30,12 @@ public class GildedRoseShould {
 		items.add(new Item("+5 Dexterity Vest", 10, 20));
 		items.add(new Item("Conjured Mana Cake", 3, 6));
 
-		items = gildedRose.updateQuality(items);
+		GildedRose.updateQuality(items);
 
-		assertEquals(items.get(0).sellIn, 9);
-		assertEquals(items.get(0).quality, 19);
-		assertEquals(items.get(1).sellIn, 2);
-		assertEquals(items.get(1).quality, 5);
-	}
-
-	@Test
-	public void increaseTheQualityByOneOfTheProductsThatGetBetterAsTheyAge() {
-		items.add(new Item("Aged Brie", 20, 30));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30));
-
-		items = gildedRose.updateQuality(items);
-
-		assertEquals(items.get(0).sellIn, 19);
-		assertEquals(items.get(0).quality, 31);
-		assertEquals(items.get(1).sellIn, 19);
-		assertEquals(items.get(1).quality, 31);
-	}
-
-	@Test
-	public void increaseQualityByTwoOfTheProductsThatGetBetterAsTheyAgeWhenThereAre10daysOrLessLeft() throws Exception {
-		items.add(new Item("Aged Brie", 10, 34));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 8, 30));
-
-		items = gildedRose.updateQuality(items);
-
-		assertEquals(items.get(0).sellIn, 9);
-		assertEquals(items.get(0).quality, 36);
-		assertEquals(items.get(1).sellIn, 7);
-		assertEquals(items.get(1).quality, 32);
-	}
-
-	@Test
-	public void increasesTheQualityByThreeOfTheProductsThatGetBetterAsTheyAge_WhenThereAre5daysOrLessLeft()
-			throws Exception {
-		items.add(new Item("Aged Brie", 4, 11));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 15));
-
-		items = gildedRose.updateQuality(items);
-
-		assertEquals(items.get(0).sellIn, 3);
-		assertEquals(items.get(0).quality, 14);
-		assertEquals(items.get(1).sellIn, 4);
-		assertEquals(items.get(1).quality, 18);
+		assertEquals(9, items.get(0).sellIn);
+		assertEquals(19, items.get(0).quality);
+		assertEquals(2, items.get(1).sellIn);
+		assertEquals(5, items.get(1).quality);
 	}
 
 	@Test
@@ -84,44 +44,89 @@ public class GildedRoseShould {
 		items.add(new Item("+5 Dexterity Vest", 0, 20));
 		items.add(new Item("Conjured Mana Cake", 0, 6));
 
-		items = gildedRose.updateQuality(items);
+		GildedRose.updateQuality(items);
 
 		assertEquals(items.get(0).sellIn, -1);
 		assertEquals(items.get(0).quality, 18);
 		assertEquals(items.get(1).sellIn, -1);
 		assertEquals(items.get(1).quality, 4);
 	}
+	
+	@Test
+	public void qualityIsNeverNegative(){
+		items.add(new Item("+5 Dexterity Vest", 10, 0));
+
+		GildedRose.updateQuality(items);
+
+		assertEquals(9, items.get(0).sellIn);
+		assertEquals(0, items.get(0).quality);
+	}
 
 	@Test
-	public void updatesTheQualityOfBackstagePassesAndBrieToZeroWhenWeHavePassedTheSellInDate() throws Exception {
-		items.add(new Item("Aged Brie", 0, 20));
-		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
+	public void increaseTheQualityByOneOfTheProductsThatGetBetterAsTheyAge() {
+		items.add(new Item("Aged Brie", 20, 30));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30));
 
-		items = gildedRose.updateQuality(items);
+		GildedRose.updateQuality(items);
 
-		assertEquals(items.get(0).sellIn, -1);
-		assertEquals(items.get(0).quality, 0);
-		assertEquals(items.get(1).sellIn, -1);
-		assertEquals(items.get(1).quality, 0);
+		assertEquals(19, items.get(0).sellIn);
+		assertEquals(31, items.get(0).quality);
+		assertEquals(19, items.get(1).sellIn);
+		assertEquals(31, items.get(1).quality);
+	}
+
+	@Test
+	public void doesNotIncreaseQualityhOver50() throws Exception {
+		items.add(new Item("Aged Brie", 3, 50));
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 20, 50));
+
+		GildedRose.updateQuality(items);
+
+		assertEquals(items.get(0).sellIn, 2);
+		assertEquals(items.get(0).quality, 50);
+		assertEquals(items.get(1).sellIn, 19);
+		assertEquals(items.get(1).quality, 50);
 	}
 
 	@Test
 	public void doesNotAlterTheQualityOfSulfurasWhichIsAlways80() throws Exception {
 		items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
 
-		items = gildedRose.updateQuality(items);
+		GildedRose.updateQuality(items);
 
 		assertEquals(items.get(0).sellIn, 0);
 		assertEquals(items.get(0).quality, 80);
 	}
 
 	@Test
-	public void doesNotIncreaseQualityhOver50() throws Exception {
-		items.add(new Item("Aged Brie", 3, 50));
+	public void increaseQualityByTwoOfBackstagePassWhenThereAre10daysOrLessLeft() throws Exception {
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 8, 30));
 
-		items = gildedRose.updateQuality(items);
+		GildedRose.updateQuality(items);
 
-		assertEquals(items.get(0).sellIn, 2);
-		assertEquals(items.get(0).quality, 50);
+		assertEquals(7, items.get(0).sellIn);
+		assertEquals(32, items.get(0).quality);
 	}
+
+	@Test
+	public void increasesTheQualityByThreeOfBackstagePassWhenThereAre5daysOrLessLeft()
+			throws Exception {
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 15));
+
+		GildedRose.updateQuality(items);
+
+		assertEquals(4, items.get(0).sellIn);
+		assertEquals(18, items.get(0).quality);
+	}
+
+	@Test
+	public void updatesTheQualityOfBackstagePassToZeroWhenWeHavePassedTheSellInDate() throws Exception {
+		items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20));
+
+		GildedRose.updateQuality(items);
+
+		assertEquals(items.get(0).sellIn, -1);
+		assertEquals(items.get(0).quality, 0);
+	}
+
 }
